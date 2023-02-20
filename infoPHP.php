0c058da93e3dc5b7b9b9c196a1da0373ed9ab181
Rizky Ananda F <rizkyanandafaradin@gmail.com>
2023-02-20 21:52:32 +0700

    Commit ke 7

--- infoPHP.php
+++ infoPHP.php
@@ -1,8 +1,8 @@
 <?php
 
-phpinfo();
 echo "Belajar MYSQL DASAR";
 echo "Belajar MysqL hari ketiga";
 echo "Belajar Mysql hari keempat";
 echo "Belajar Mysql hari kelima";
 echo "belajar Mysql hari keenam";
+echo "Belajar interview"
